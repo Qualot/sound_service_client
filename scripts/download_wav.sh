bdeb514daa7e3cc77adc2cdf4909a5f747e85829
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2024-02-11 18:01:20 +0900

    add shell script to download wav files

---
+++ scripts/download_wav.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+wget -O `(rospack find sound_service_client)`/wav/tm2r_shoot15.wav "https://osabisi.sakura.ne.jp/m2/tm4/se/tm2r_shoot15.wav"
+wget -O `(rospack find sound_service_client)`/wav/chu1.wav "https://web.archive.org/web/20070307170329/http://utm-game-web.hp.infoseek.co.jp/sound/sound3/chu1.wav"
